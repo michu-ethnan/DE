f4f9ea056647ecf6db39734069e536bc06b26e3b
Abi <tekasimeon22@gmail.com>
2022-04-21 15:37:29 +0200

    release 3.37.0

--- src/test/java/com/deosite/tests/features/creating_orders/OrderProductsFromCategories.java
+++ src/test/java/com/deosite/tests/features/creating_orders/OrderProductsFromCategories.java
@@ -66,7 +66,7 @@ public class OrderProductsFromCategories {
 @When("she adds a product from category page")
     public void alex_adds_product_from_category_page(){
     theActorInTheSpotlight().attemptsTo(
-            Open.productPageByPosition(1),
+            Open.productPageByPosition(3),
             AddProduct.toCart(),
             Open.miniCart()
 
